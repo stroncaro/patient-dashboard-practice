3159f121387b47392a7801ec9d366e7f385bd696
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-07-21 15:04:16 -0300

    Add basic modal layout

--- src/App.tsx
+++ src/App.tsx
@@ -54,16 +54,75 @@ const Header: React.FC = () => {
 
 const Content: React.FC = () => {
   const [patients, setPatients] = useState<PatientList>(INITIAL_PATIENT_LIST);
+  const [selectedPatientIndex, setSelectedPatientIndex] = useState<number | null>(null);
   
+  const closeModal: () => void = () => {
+    setSelectedPatientIndex(null);
+  }
+
   return (
     <main>
       <h1>Patients</h1>
       <ul>
         {patients.map((patient, i) => 
-          <li key={i}>{patient.lastName}, {patient.name} {patient.middleName}</li>
+          <li key={patient.id} onClick={() => setSelectedPatientIndex(i)}>
+            {patient.lastName}, {patient.name} {patient.middleName}
+          </li>
         )}
       </ul>
+      {selectedPatientIndex !== null && (
+        <PatientModal patient={patients[selectedPatientIndex]} close={closeModal}/>
+      )}
     </main>
+  )
+}
+
+interface PatientModalProps {
+  patient: Patient;
+  close: () => void;
+  state?: PatientModalState;
+}
+
+type PatientModalState = 'show' | 'edit';
+
+const PatientModal: React.FC<PatientModalProps> = ({ patient, state, close }) => {
+  const [modalState, setModalState] = useState<PatientModalState>(state || 'show');
+
+  return (
+    <div className='modal-container'>
+      <div className='patient-modal-box'>
+        <div className='modal-header'>
+          <button onClick={close}>X</button>
+        </div>
+        <div className='modal-body'>
+          <div>
+            <p>Name:</p>
+            <p>Middle Name:</p>
+            <p>Last Name:</p>
+            <p>Age:</p>
+            <p>Phone:</p>
+            <p>Mail:</p>
+            <p>Address:</p>
+          </div>
+          <div>
+            <p>{patient.name}</p>
+            <p>{patient.middleName}</p>
+            <p>{patient.lastName}</p>
+            <p>{patient.age}</p>
+            <p>{patient.phone}</p>
+            <p>{patient.mail}</p>
+            <p>{patient.address}</p>
+          </div>
+        </div>
+        <div className='modal-button-container'>
+          {modalState === 'show' 
+            ? <button onClick={() => setModalState('edit')}>Edit</button>
+            : <button>Save</button>
+          }
+          <button onClick={close}>Cancel</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
