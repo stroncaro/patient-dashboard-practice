8660e1d2aebabe405c6e5fe25c6d872a8ef955cf
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-07-22 21:52:02 -0300

    Add style to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,19 +5,43 @@ export const Header: React.FC = () => {
   const { isLoggedIn, logIn, logOut } = useContext(AuthContext);
 
   return (
-    <header>
-      <div className='company-name'>
-        <div className='logo'>S</div>
+    <header className="flex justify-between items-center p-4 bg-green-500 text-white">
+      <div className="flex items-baseline text-2xl font-bold gap-1 underline">
+        <div className="bg-white text-green-500 p-2 rounded-full size-8 flex justify-center items-center">S</div>
         SuperSoft
       </div>
-      <ul>
+      <ul className="flex items-center gap-4 font-bold">
         {!isLoggedIn ? (
           <>
-            <li><button onClick={logIn}>Log In</button></li>
-            <li><button onClick={logIn}>Sign up</button></li>
+            <li>
+              <button
+                onClick={logIn}
+                className=
+                  "border rounded w-24 h-10"
+              >
+                Log In
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={logIn}
+                className=
+                  "border rounded w-24 h-10 bg-blue-600"
+              >
+                Sign up
+              </button>
+            </li>
           </>
         ) : (
-          <li><button onClick={logOut}>Log out</button></li>
+          <li>
+            <button
+              onClick={logOut}
+              className=
+                "border rounded w-24 h-10"
+            >
+              Log out
+            </button>
+          </li>
         )}
       </ul>
     </header>
