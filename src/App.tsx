a2f01789c7cdb5b1cb17944ab5a36a536a384b86
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-08-02 02:56:24 -0300

    Add login route to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import { AuthProvider } from './contexts/AuthContext';
-import AppContent from './components/AppContent';
-import Header from './components/Header';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import AppContent from "./components/AppContent";
+import Header from "./components/Header";
+import LogInForm from "./components/users/LogInForm";
 
 function App() {
   return (
     <AuthProvider>
-      <Header />
-      <AppContent />
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <AppContent />
+              </>
+            }
+          />
+          <Route path="/login" element={<LogInForm />} />
+        </Routes>
+      </Router>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
