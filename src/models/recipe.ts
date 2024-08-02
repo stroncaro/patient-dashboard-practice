ba6c55deae82bf61464d865b61e822a08aa67cd0
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-08-02 19:07:13 -0300

    Add recipe model

---
+++ src/models/recipe.ts
@@ -0,0 +1,35 @@
+export type RecipeList = Recipe[];
+
+interface IRecipe {
+  userId: number;
+  patientId: number;
+  content: string;
+}
+
+class Recipe implements IRecipe {
+  id: number;
+  userId: number;
+  patientId: number;
+  content: string;
+
+  constructor(id: number, user: number, patient: number, content: string) {
+    if (!Number.isInteger(id) || id < 0) {
+      throw new Error(`Invalid id: ${id}`);
+    }
+
+    if (!Number.isInteger(user) || user < 0) {
+      throw new Error(`Invalid user id: ${user}`);
+    }
+
+    if (!Number.isInteger(patient) || patient < 0) {
+      throw new Error(`Invalid patient id: ${patient}`);
+    }
+
+    this.id = id;
+    this.userId = user;
+    this.patientId = patient;
+    this.content = content;
+  }
+}
+
+export default Recipe;
