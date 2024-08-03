c160f0c11915bf59c545692b831ea4c8755cc98f
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-08-03 18:06:02 -0300

    Expand RecipeDashboard to get and handle recipes

--- src/components/recipes/RecipeDashboard.tsx
+++ src/components/recipes/RecipeDashboard.tsx
@@ -1,23 +1,57 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
+import { RecipeList } from "../../models/recipe";
+import useRecipes from "../../hooks/recipes/useRecipes";
 
 const RecipeDashboard: React.FC = () => {
   const { user } = useContext(AuthContext);
 
+  const { getRecipes } = useRecipes();
+  const [recipes, setRecipes] = useState<RecipeList>([]);
+  const [working, setWorking] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (user) {
+      setWorking(true);
+      getRecipes(user.id as number)
+        .then((recipes) => setRecipes(recipes))
+        .catch((error) => console.error(error))
+        .finally(() => setWorking(false));
+    }
+  }, []);
+
   return (
-    <>
-      {user ? (
-        <div>
-          {/* TODO */}
-          Recipe Dashboard
-        </div>
-      ) : (
-        <div className="m-4">
-          <h1 className="text-2xl font-bold">Error: content not accessible</h1>
+    <div className="w-1/2">
+      {!user && (
+        <>
+          <div className="my-4">
+            <h1 className="text-3xl font-bold">Content not accessible</h1>
+            <hr />
+          </div>
           <p>Must be logged in</p>
-        </div>
+        </>
       )}
-    </>
+      {user && (
+        <>
+          <div className="my-4">
+            <h1 className="text-3xl font-bold">Your recipes</h1>
+            <hr />
+          </div>
+          <div>
+            {working && <p>Loading...</p>}
+            {!working && recipes.length === 0 && <p>You have no recipes</p>}
+            {!working && recipes.length > 0 && (
+              <ul>
+                {recipes.map((recipe, i) => (
+                  // TODO: properly render recipes, grouped by patient
+                  <li key={i}>{recipe.content}</li>
+                ))}
+              </ul>
+            )}
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 
