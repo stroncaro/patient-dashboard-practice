dcc2e8d0a135b935dac81085679cc85e04a4e23a
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-08-03 16:44:01 -0300

    Add routing to use Landing and MainLayout components

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
-import AppContent from "./components/AppContent";
-import Header from "./components/Header";
 import LogInForm from "./components/users/LogInForm";
 import SignUpForm from "./components/users/SignUpForm";
 import RecipeDashboard from "./components/recipes/RecipeDashboard";
+import PatientDashboard from "./components/patients/PatientDashboard";
+import MainLayout from "./components/MainLayout";
+import Landing from "./components/Landing";
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path="*" element={<p>404: Not Found</p>} />
-          <Route
-            path="/"
-            element={
-              <>
-                <Header />
-                <AppContent />
-              </>
-            }
-          />
-          <Route path="/user/recipes" element={<RecipeDashboard />} />
           <Route path="/login" element={<LogInForm />} />
           <Route path="/signup" element={<SignUpForm />} />
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Landing />} />
+            <Route path="patients" element={<PatientDashboard />} />
+            <Route path="recipes" element={<RecipeDashboard />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
     </AuthProvider>
